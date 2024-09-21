8be3dab0f01e709d95967ee412a7b745589d4a05
kimmokalover <kimmokalover@gmail.com>
2024-09-21 17:23:16 +0900

    init : spring boot server initialized

---
+++ rubbish-server/src/main/java/rubbish/rubbish_server/RubbishServerApplication.java
@@ -0,0 +1,13 @@
+package rubbish.rubbish_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubbishServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubbishServerApplication.class, args);
+	}
+
+}
---
+++ rubbish-server/src/test/java/rubbish/rubbish_server/RubbishServerApplicationTests.java
@@ -0,0 +1,13 @@
+package rubbish.rubbish_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubbishServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
